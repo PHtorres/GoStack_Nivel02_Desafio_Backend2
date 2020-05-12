51dab5fbf481825fb5546e01030bad2e5f93194c
PHtorres <58959268+PHtorres@users.noreply.github.com>
2020-05-12 00:16:59 -0300

    parte da importação

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,45 @@
+import csvparse from 'csv-parse';
+import fs from 'fs';
 import Transaction from '../models/Transaction';
 
+interface LineCSVDTO {
+  title: string;
+  type: string;
+  value: number;
+  category: string;
+}
+
+
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+
+  async execute(filePath: string): Promise<Transaction[]> {
+    const contactsReadStream = fs.createReadStream(filePath);
+    const parsers = csvparse({
+      from_line: 2,
+    });
+
+    const parseCSV = contactsReadStream.pipe(parsers);
+
+    const transactions = new Array<LineCSVDTO>();
+    const categories = new Array<string>();
+
+    parseCSV.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim());
+
+      if (!title || !type || !value) return;
+
+      categories.push(category);
+      transactions.push({ title, type, value, category });
+    });
+
+    await new Promise(resolve => parseCSV.on('end', resolve));
+
+    console.log(transactions);
+    console.log(categories);
+
+    return new Array<Transaction>();
+
   }
 }
 
