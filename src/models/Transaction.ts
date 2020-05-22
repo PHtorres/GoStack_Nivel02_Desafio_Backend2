4ff59af3ecb5bc6b682f8e6d0fb5077eeb111efc
PHtorres <58959268+PHtorres@users.noreply.github.com>
2020-05-22 19:25:38 -0300

    alterações para site gofinances

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -17,7 +17,7 @@ class Transaction {
   @Column('decimal')
   value: number;
 
-  @ManyToOne(() => Category)
+  @ManyToOne(() => Category, category => category.transaction, {eager: true})
   @JoinColumn({ name: 'category_id' })
   category:Category
 
